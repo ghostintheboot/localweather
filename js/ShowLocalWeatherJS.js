234266c62b4587e748cfe7ec956fd64f10a1b498
ghostintheboot <17715289+ghostintheboot@users.noreply.github.com>
2020-04-27 20:57:44 -0500

    Update ShowLocalWeatherJS.js

--- js/ShowLocalWeatherJS.js
+++ js/ShowLocalWeatherJS.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
   $(".weather-icon").html('<img src="http://openweathermap.org/img/w/01d.png">');
 
   // Get geolocation.
-  if (navigator.geolocation) {
+  // TEST. Force geolocation.
+//   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
       long = position.coords.longitude;
@@ -109,5 +110,5 @@ $(document).ready(function() {
       $(".fade").hide(0).fadeIn(1250);
 
     }); // navigator.geolocation.getCurrentPosition end.
-  } // navigator.geolocation end.
+//   } // navigator.geolocation end.
 });
